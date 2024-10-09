49d8044c0b2c44a1c5ba5965d5ff2530b8e4bcc0
Andrii Dudko <Annbpiu@gmail.com>
2024-10-09 09:37:33 +0300

    make build

---
+++ index.af22fa66.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("div"),t=document.querySelector("img");e.style.display="flex",e.style.justifyContent="center",e.style.alignItems="center",t.style.position="relative";
+//# sourceMappingURL=index.af22fa66.js.map
